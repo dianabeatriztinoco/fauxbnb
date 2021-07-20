757a0fc08e40412121186adce87ddbbd4a3c0e5b
Diana Beatriz Tinoco <dianabeatriztinoco@gmail.com>
2021-07-19 20:03:44 -0500

    more styling to landing page

--- frontend/src/components/LandingPage/index.js
+++ frontend/src/components/LandingPage/index.js
@@ -4,17 +4,27 @@ import './LandingPage.css'
 
 const LandingPage = () => {
     return (
-        <h1>
+        
         <body>
-        <div id='showcase'>
-        </div> 
-    
+
+        <div id='showcase'></div> 
+        <form className="formContainer">
+        <div className="locationContainer">
         <label>location</label>
-        <input placeholder="Nearby">
-    
-        </input>
+        <input className="focus:outline-none" placeholder="Nearby"></input>
+        </div>
+        <div className='check-inContainer'>
+        <label>check-in</label>
+        <input className="noOutline" placeholder="Add Date"></input>
+        </div>
+        <div className='check-outContainer'>
+        <label>check-out</label>
+        <input className="noOutline" placeholder="Add Date"></input>
+        </div>
+        </form>
+
         </body> 
-        </h1>
+   
     )
 }
 
